78ca7c5c8d9340e9ef3ec6ecb4134b8892d68834
Andrew Berezovskyi <andrew@berezovskyi.me>
2025-06-19 01:00:16 +0200

    test: update

--- src/test/kotlin/me/berezovskyi/corsherdf/corsherdfweb/CorsherdfWebApplicationTests.kt
+++ src/test/kotlin/me/berezovskyi/corsherdf/corsherdfweb/CorsherdfWebApplicationTests.kt
@@ -10,14 +10,39 @@ import javax.ws.rs.core.MediaType // Added for MediaType.TEXT_PLAIN
 @QuarkusTest
 class CorsherdfWebApplicationTests {
 
+
     @Test
-    fun testRProxyEndpointWithHtmlTarget() {
-        val remoteUrl = "http://example.com"
+    fun testRProxyEndpointWithHtmlTarget2() {
+        val remoteUrl = "http://purl.org/dc/terms"
         given()
           .`when`().get("/r/$remoteUrl")
           .then()
              .log().ifValidationFails() // Log response body if test fails
              .statusCode(422) // Expecting Unprocessable Entity as example.com returns HTML
+             .body(`is`("Server did not return any RDF, got HTML instead from $remoteUrl"))
+    }
+
+
+    @Test
+    fun testRProxyEndpointWithHtmlTarget() {
+        val remoteUrl = "https://example.com/"
+        given()
+          .`when`().get("/r/$remoteUrl")
+          .then()
+             .log().ifValidationFails() // Log response body if test fails
+             .statusCode(422) // Expecting Unprocessable Entity as example.com returns HTML
+             .body(`is`("Server did not return any RDF, got HTML instead from $remoteUrl"))
+    }
+
+
+    @Test
+    fun testRProxyEndpointWithRdfTarget) {
+        val remoteUrl = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+        given()
+          .`when`().get("/r/$remoteUrl")
+          .then()
+             .log().ifValidationFails() // Log response body if test fails
+             .statusCode(200) // Expecting Unprocessable Entity as example.com returns HTML
              .body(`is`("Server did not return any RDF, got HTML instead from $remoteUrl"))
     }
 
